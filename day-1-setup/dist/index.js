36bf971c512b3283e9e96054413abc33988229ce
SachinDevTech <sachin.1107sk@gmail.com>
2026-01-06 00:41:59 +0530

    Added TS Setup Code

---
+++ day-1-setup/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function greet(nameOfPerson) {
+    return `Welcome my  brother: ${nameOfPerson}`;
+}
+const nameYJS = "YJS";
+console.log(greet(nameYJS));
+//# sourceMappingURL=index.js.map 
---
+++ day-1-setup/src/index.ts
@@ -0,0 +1,5 @@
+function greet(nameOfPerson:string) {
+    return `Welcome my  brother: ${nameOfPerson}`;
+}
+const nameYJS:string = "YJS"; 
+console.log (greet(nameYJS))
